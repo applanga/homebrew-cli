99cb83242430f178ac4d2203b9ba79ac6f740321
Steffen Römer <steffen@applanga.com>
2018-06-22 15:47:44 +0200

    Applanga Command Line Interface Version 1.0.26

---
+++ Formula/applanga.rb
@@ -0,0 +1,17 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Applanga < Formula
+  desc "Applanga Command Line Interface (CLI)"
+  homepage "https://www.applanga.com/docs/cli"
+
+  version "1.0.26"
+  sha256 "5b6e98aea4461a5f41d41c39487cf1a2b206589c3fb1b31d993c5c7285250e56"
+
+  url "https://github.com/applanga/applanga-cli/releases/download/#{version}/applanga_osx.tar.gz"
+
+  def install
+    bin.install "applanga"
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# homebrew-cli
+# Homebrew tap for Applanga Command Line Interface (CLI)
+Add the tap to your local homebrew installation:
+
+		brew tap applanga/cli
+
+Install applanga cli:
+
+		brew install applanga
+
