6e39d60d11efde8e2d37e0ca757c815b3fe1e07b
Jenkins <jenkins@Jenkinss-MBP.fritz.box>
2022-03-17 13:46:16 +0100

    Applanga Command Line Interface Version 1.0.74

--- Formula/applanga.rb
+++ Formula/applanga.rb
@@ -6,8 +6,8 @@ class Applanga < Formula
   desc "Applanga Command Line Interface (CLI)"
   homepage "https://www.applanga.com/docs-integration/cli"
 
-  version "1.0.73"
-  sha256 "30851c18ba31a5b52f8aa37d71058b82c003ee7506a26f98095506b5ba23476b"
+  version "1.0.74"
+  sha256 "30cb87c239edd64d99eb22cd25dc3bf7aad1d32860bbca2025bf91f9b27a0b46"
 
   url "https://github.com/applanga/applanga-cli/releases/download/#{version}/applanga_osx.tar.gz"
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Homebrew tap for Applanga Localization Command Line Interface (CLI)
 
 ***
-*Version:* 1.0.73
+*Version:* 1.0.74
 
 *Website:* <https://www.applanga.com>
 
