0586028dbfed7b6ef33b96cbc5d9f1117ca8e42f
Jenkins <info@applanga.com>
2023-05-25 13:58:46 +0200

    Applanga Command Line Interface Version 1.0.78

--- Formula/applanga.rb
+++ Formula/applanga.rb
@@ -6,8 +6,8 @@ class Applanga < Formula
   desc "Applanga Command Line Interface (CLI)"
   homepage "https://www.applanga.com/docs-integration/cli"
 
-  version "1.0.77"
-  sha256 "789768db2070b828fa6cc9363cb765d39db571569474ff59fd29dbca0f3b24cb"
+  version "1.0.78"
+  sha256 "5e3e8bf4b0051a0dbac93e0d95bf61fbdbaba697e7bc2033ed46d85f33b66bcb"
 
   url "https://github.com/applanga/applanga-cli/releases/download/#{version}/applanga_osx.tar.gz"
 
