9834ba932b1ef219057f5e71cfdf37f4188cbf00
steffen-roemer <>
2024-11-21 14:09:39 +0100

    Applanga Command Line Interface Version 1.0.102

--- Formula/applanga.rb
+++ Formula/applanga.rb
@@ -6,8 +6,8 @@ class Applanga < Formula
   desc "Applanga Command Line Interface (CLI)"
   homepage "https://www.applanga.com/docs-integration/cli"
 
-  version "1.0.101"
-  sha256 "5d4fdfc35dd06dee528772bb666221356f52ef1f28c2d12f06842a5576f788e6"
+  version "1.0.102"
+  sha256 "96d4ec4185d9c376034d3e18a5cf739496c639ddf94ece4e6d0910d05b87d21c"
 
   url "https://github.com/applanga/applanga-cli/releases/download/#{version}/applanga_osx.tar.gz"
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Homebrew tap for Applanga Localization Command Line Interface (CLI)
 
 ***
-*Version:* 1.0.101
+*Version:* 1.0.102
 
 *Website:* <https://www.applanga.com>
 
